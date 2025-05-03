08a5561b0c8fb03d019417bd1bfea0bf91d479a4
mitulty <mitultyagi45@gmail.com>
2025-05-03 16:05:18 +0530

    Hello World Using Cargo
    Signed-off-by: mitulty <mitultyagi45@gmail.com>

---
+++ 0_hello_world/src/main.rs
@@ -0,0 +1,17 @@
+/* @Author: Mitul
+   @Description: This is a simple Rust program that prints "Hello, world!" to the console.
+*/
+/*
+Follow these steps to create a new Rust project using Cargo:
+- cargo new hello_world
+- cd hello_world
+- cargo run
+- cargo build
+- cargo check
+- cargo test
+- cargo doc
+- cargo fmt
+*/
+fn main() {
+    println!("Hello, world!");
+}
